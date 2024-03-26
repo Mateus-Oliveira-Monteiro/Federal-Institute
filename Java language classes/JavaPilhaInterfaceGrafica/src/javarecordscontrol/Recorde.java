c76360a41b61e6574cb95862ca3270e03a5f551e
Mateus-Monteiro <mateusomont@gmail.com>
2024-03-25 22:26:11 -0300

    Add files via upload

---
+++ Java language classes/JavaPilhaInterfaceGrafica/src/javarecordscontrol/Recorde.java
@@ -0,0 +1,44 @@
+package javarecordscontrol;
+
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class Recorde {
+        private LocalDate dataRecorde;
+        private String nome;
+        private double tempo;
+
+    public LocalDate getDataRecorde() {
+        return dataRecorde;
+    }
+
+    public void setDataRecorde(LocalDate dataRecorde) {
+        this.dataRecorde = dataRecorde;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getTempo() {
+        return tempo;
+    }
+ 
+    public void setTempo(double tempo) {
+        this.tempo = tempo;
+    }
+    
+    @Override
+    public String toString() {
+     DateTimeFormatter format = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        LocalDate data = dataRecorde;
+    return "Recorde [nome=" + nome + ", dataRecorde=" + dataRecorde + ", tempo=" + tempo + " segundos]";
+}
+            
+
+}
