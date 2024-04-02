c59ffc841781e2b4d54fbf4800a07845177fcea8
Mateus-Monteiro <mateusomont@gmail.com>
2024-04-01 22:34:40 -0300

    Add files via upload

---
+++ Java language classes/Interfaces/src/Circulo.java
@@ -0,0 +1,10 @@
+class Circulo implements AreaCalculavel {
+    private int raio;
+    public Circulo(int raio) {
+        this.raio = raio;
+    }
+    @Override
+    public double calculaArea() {
+        return Math.PI * raio * raio;
+    }
+}
